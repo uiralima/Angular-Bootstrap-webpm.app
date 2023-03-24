665585bc770ac662b3ec762d526b31513fec326c
Uira Tupinambas Lima <uiralima@gmail.com>
2023-03-24 20:14:06 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,13 +53,13 @@ import { FirebaseActivityService } from './services/implementations/activity.fir
     ReactiveFormsModule,
     FormsModule,
     AngularFireModule.initializeApp({
-      apiKey: "AIzaSyAAvFoh7c9jarFNac2E9PBrbaV1ezrcoOA",
-      authDomain: "webpm-73a86.firebaseapp.com",
-      databaseURL: "https://webpm-73a86.firebaseio.com",
-      projectId: "webpm-73a86",
-      storageBucket: "webpm-73a86.appspot.com",
-      messagingSenderId: "1065143550265",
-      appId: "1:1065143550265:web:8783f974e63d75023fe8e8"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: ""
     }),
     RouterModule.forRoot(ROUTES)
   ],
